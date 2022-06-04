71df7d11b4659ab733c2b9c0f07e3a5791330e86
John-sys <gbayakokoyajohnjr@gmail.com>
2022-06-04 18:21:00 +0000

    new commut

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typed from "react-typed";
+// import Typed from "react-typed";
 
 const Hero = () => {
   return (
@@ -9,7 +9,7 @@ const Hero = () => {
           <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl">Growth in Business</h1>
           <div className="flex justify-center items-center">
               <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Websites for</p>
-              <Typed strings={['Everyone', 'Everybody', 'Everywhere']} typedSpeed={120} backSpeed={140} loop className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 md:pl-4 text-gray" />
+              {/* <Typed strings={['Everyone', 'Everybody', 'Everywhere']} typedSpeed={120} backSpeed={140} loop className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 md:pl-4 text-gray" /> */}
           </div>
           <p className="md:text-2xl text-xl font-bold text-[#6c757d] ">Building websites for every available business market and creating opportunities for entrepreneurs</p>
           <button className=" bg-[#00df9a] w-[200px] rounded-md font-medium my-2 text-black hover:bg-gray mx-auto py-3 ">Get Started</button>
