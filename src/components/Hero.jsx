f3cb45abef88a6251ea8f50362ebaf49aca4db60
John-sys <gbayakokoyajohnjr@gmail.com>
2022-06-05 18:12:00 +0000

    UI changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
   return (
     <div className="text-white  ">
       <div className="max-w-[800px]  w-full h-screen mx-auto flex flex-col text-center justify-center  ">
-          <p className=" uppercase text-[#00df9a] font-bold p-2 ">Access to the best designs</p>
-          <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl">Growth in Business</h1>
+          <p className=" uppercase text-[#00df9a] font-bold p-2 ">LETS BUILD SOMETHING TOGETHER</p>
+          <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl">Hi, I'm John</h1>
           <div className="flex justify-center items-center">
-              <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Websites for</p>
+              <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">A Front-End Web Developer </p>
               {/* <Typed strings={['Everyone', 'Everybody', 'Everywhere']} typedSpeed={120} backSpeed={140} loop className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 md:pl-4 text-gray" /> */}
           </div>
-          <p className="md:text-2xl text-xl font-bold text-[#6c757d] ">Building websites for every available business market and creating opportunities for entrepreneurs</p>
+          <p className="md:text-2xl text-xl font-bold text-[#6c757d] ">I'm a front-end web developer specializing in building exceptional digital experiences.</p>
           <button className=" bg-[#00df9a] w-[200px] rounded-md font-medium my-2 text-black hover:bg-gray mx-auto py-3 ">Get Started</button>
       </div>
     </div>
