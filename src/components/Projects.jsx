5c54b2c9772e01028ed2cbcb27bae906901c300a
John-sys <gbayakokoyajohnjr@gmail.com>
2022-06-08 13:32:37 +0000

    Ui improvement

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div className='w-full py-2 px-4 bg-white'>
+    <div className="w-full py-2 px-4 ">
+      <div className="max-w-[1240px] md:mx-auto px-5">
 
+        {/* Title */}
+        <div className="space-y-2 my-[30px] w-[250px] md:ml-[5rem]">
+          <p className="text-[#00df9a] text-[20px] ">PROJECTS</p>
+          <h1 className="text-2xl font-bold">What I've worked on</h1>
+        </div>
+
+
+        <div className="">
+
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
